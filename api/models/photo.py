e0656a743e03820900e9956144b5ae9014af0632
esalling23 <esalling23@gmail.com>
2020-11-25 22:08:30 -0500

    Compresses images before saving to s3 bucket
    Follows code outlined in  https://medium.com/@mahmudtopu3/compress-images-in-the-background-using-python-django-8ec4df7cad3c

--- api/models/photo.py
+++ api/models/photo.py
@@ -1,4 +1,15 @@
 from django.db import models
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
+
+# image compression method
+def compress(image):
+    im = Image.open(image)
+    im_io = BytesIO()
+    im.save(im_io, 'JPEG', quality=60)
+    new_image = File(im_io, name=image.name)
+    return new_image
 
 class Photo(models.Model):
     file = models.FileField()
@@ -6,3 +17,9 @@ class Photo(models.Model):
     def __str__(self):
         print(self.file)
         return self.file.name
+
+    # calling image compression function before saving the data
+    def save(self, *args, **kwargs):
+        new_image = compress(self.file)
+        self.file = new_image
+        super().save(*args, **kwargs)
